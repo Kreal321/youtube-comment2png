27dadb66eace88407f399746f5a71af24fe39068
Kreal321 <kezhen_xu@hotmail.com>
2018-03-14 13:12:10 -0400

    init commit

--- script.js
+++ script.js
@@ -1,49 +1,55 @@
 $(document).ready(function(){
-    var id = 1; //image id
-    var zip = new JSZip(); // zip
 
-    // cancas image load
+    var id = 1; // image id
+    var zip = new JSZip(); // zip package
+
+    // load canvas
     function loadimage(id,i) {
-        var width = id.offsetWidth; //dom宽
-        var height = id.offsetHeight; //dom高
-        // 解决图片模糊
-        var scale = window.devicePixelRatio * 2;//放大倍数
-        var canvas = document.createElement('canvas');
+        var width = id.offsetWidth; //dom width
+        var height = id.offsetHeight; //dom height
+
+        // fix blurred picture
+        var scale = window.devicePixelRatio * 2; // 2x device pixel
+        var canvas = document.createElement('canvas'); // create canvas
         canvas.width = width * scale;
         canvas.height = height * scale;
         canvas.style.width = width + 'px';
         canvas.style.height = height + 'px';
 
-
+        // fix picture offset
         //设置context位置，值为相对于视窗的偏移量负值，让图片复位(解决偏移的重点)
-        var rect = id.getBoundingClientRect();//获取元素相对于视察的偏移量
+        var rect = id.getBoundingClientRect(); // get object position relative to the viewport
 
         canvas.getContext("2d").scale(scale, scale);
         canvas.getContext("2d").translate(-rect.left, -rect.top);
 
         var opts = {
             canvas: canvas,
-            useCORS: true, // 【重要】开启跨域配置
-            scrollY: 0, // 纵向偏移量 写死0 可以避免滚动造成偶尔偏移的现象
+            useCORS: true, // Open cross domain
+            scrollY: 0, // Y offset = 0  Avoid deviation caused by rolling
         };
+
+        // get png
         html2canvas(id, opts).then(function(canvas) {
-            // append image to body
+            // append image to body (debug)
             //document.body.appendChild(canvas);
 
-            // download
+            // png file name
             var name = $("#C"+i).text();
+
+            // Single download
             //saveAs(canvas.toDataURL(), name);
 
-            // zip package
+            // zip download
             package(canvas.toDataURL(), name,i);
 
-            console.log("canvas "+i+"/"+id);
-
+            // console debug
+            //console.log("canvas "+i+"/"+id);
 
         },1000);
     }
 
-    // save as download
+    // single download (not use)
     function saveAss(uri, filename) {
 
         var link = document.createElement('a');
@@ -67,17 +73,21 @@ $(document).ready(function(){
 
     // JS zip
     function package(url,filename,i){
-
+        // package png file
         zip.file(filename+".png", url.split(';base64,')[1], {base64: true});
-        console.log("zip "+i+"/"+id);
+        // debug message
+        console.log("Comment " + i + " start packaging");
 
-
+        // zip download
         if(i == (id-1)){
             zip.generateAsync({type:"blob"}).then(function(content) {
                 saveAs(content, "example.zip");
             });
-            console.log("zip download");
+            // debug message
+            console.log((id -1) + " comments packaged");
+            console.log("zip downloaded");
         }
+
 
         if(i != (id-1)){
             i++;
@@ -86,7 +96,25 @@ $(document).ready(function(){
 
     }
 
-    // Step 1: connect to Youtube API
+    // progress bar
+    function process(i){
+        var process = i / (id - 1) * 100;
+        var width = process.toFixed(2) + "%";
+        $("#loadProcess").css("width",width);
+        $("#loadProcess").html(width);
+    }
+
+    // canvas process
+    function idi(i){
+        loadimage(document.getElementById(i),i);
+        // debug message
+        console.log("Comment "+ i +" start rendering");
+        // process bar
+        process(i);
+
+    }
+
+    // Step 1: load comments
     $("#load").submit(function(event){
 
         event.preventDefault();
@@ -95,17 +123,19 @@ $(document).ready(function(){
         var commentNum = $("#commentNum").val();
         var order = $("#order").val();
         var result = "";
-        var key = "AIzaSyBg4vDgPr6uFZqwg2L6BvVovASTQGm7Nh4";
-        var pageToken ="";
+        var key = "AIzaSyBg4vDgPr6uFZqwg2L6BvVovASTQGm7Nh4"; // youtube api key
+        var pageToken =""; // next page key
 
 
         while(commentNum > 0){
+            // get number of comments to take, max 100
             var number = 0;
             if(commentNum > 100){
                 number = 100;
             }else{
                 number = commentNum;
             }
+            // connect to Youtube API
             if(pageToken.length == 0){
                 var url = "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=" + videoId + "&maxResults=" + number + "&order=" + order + "&key=" + key;
             }else{
@@ -113,8 +143,9 @@ $(document).ready(function(){
 
             }
             $.get(url,function(data){
+                // debug
                 console.log(data);
-
+                // load comments
                 for(var i = 0; i < data.items.length; i++){
                     result = `
 
@@ -134,32 +165,22 @@ $(document).ready(function(){
                     $("#result").append(result);
                     id++;
                 }
-                pageToken =data.nextPageToken;
-                alert(pageToken);
+                pageToken = data.nextPageToken;
+                // debug message
+                console.log(number + " comments loaded.");
+                //console.log("pageToken: " + pageToken);
+
             })
+            // number of comments left
             commentNum = commentNum - 100;
         }
+        // hide button
         $("#loadSubmit").hide();
 
     })
 
 
-    function process(i){
-        var process = i / (id - 1) * 100;
-        var width = process.toFixed(2) + "%";
-        $("#loadProcess").css("width",width);
-        $("#loadProcess").html(width);
-    }
-
-
-    function idi(i){
-        loadimage(document.getElementById(i),i);
-
-        console.log(i+" idi");
-        process(i);
-
-    }
-
+    // Step 2: package and download
     $("#download").submit(function(event){
 
         event.preventDefault();
@@ -168,14 +189,7 @@ $(document).ready(function(){
 
         idi(i);
 
-        // for(var i = 1; i <= id; i++){
-        //     loadimage(document.getElementById(i),i);
 
-
-        // }
-
-        //zipDownload(zip);
-        //alert("y");
     })
 
 
